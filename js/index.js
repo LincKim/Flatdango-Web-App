c06645aaa7598b7b5ef294c4d616d438389b68eb
LincKim <lincolnkimutai@gmail.com>
2022-12-12 13:17:03 +0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+const MOVIES = 'http://localhost:3000/films'
+
 document.addEventListener('DOMContentLoaded', () => {
-        const defaultMovie = (poster, title, runtime, showtime, tickets_sold, capacity) => {
+    
+        function defaultMovie(poster, title, runtime, showtime, tickets_sold, capacity) {
             const movieOne = document.createElement('div')
+            movieOne.classList.add('card', 'col-12')
             movieOne.className = 'topMovie'
 
             const moviePoster = document.createElement('img')
@@ -23,7 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     buyingTicket.className = 'btn'
                     buyingTicket.textContent = `buy ticket`
 
-                    buyingTicket.addEventListener('click', () => {
+                    buyingTicket.addEventListener('click', (e) => {
+                        e.preventDefault()
                         const payTicket = document.createElement('form')
                         
                         const inputOne = document.createElement('input')
@@ -38,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         const submitButton = document.createElement('button')
                         submitButton.className = 'btn'
                         submitButton.innerText = `submit`
-                        submitButton.addEventListener('click', () => {alert('You will receive a confirmation SMS shortly')})
+                        submitButton.addEventListener('submit', (e) => alert('You will receive a confirmation SMS shortly'))
 
                         payTicket.appendChild(inputOne)
                         payTicket.appendChild(inputTwo)
@@ -57,7 +62,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 movieOne.appendChild(movieShowtime)
                 movieOne.appendChild(movietickets_sold)
 
-                document.getElementById('main').appendChild(movieOne)
+                // document.getElementById('main').appendChild(movieOne)
             }
-        }
+    function getMovies() {
+     fetch(MOVIES)
+        .then((res) => res.json())
+        .then((data) => {
+            const filmData = data.film[0]
+            const poster = filmData.poster
+            const title = filmData.title
+            const runtime = filmData.runtime
+            const showtime = filmData.showtime
+            const tickets_sold = filmData.tickets_sold
+            const filmElement = defaultMovie(poster, title, runtime, showtime, tickets_sold, capacity)
+            document.getElementById('main').appendChild(filmElement)
+        })
+    }
+    function initialize() {
+        getMovies()
+        
+    }  
+    initialize()
+}  
+defaultMovie()
+
+   
 })
